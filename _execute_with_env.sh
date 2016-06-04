3fb59d6a815d5d307f187fb22baef7827bcfdc18
matsu-chara <matsuy00@gmail.com>
2016-06-04 18:50:24 +0900

    move line

--- _execute_with_env.sh
+++ _execute_with_env.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+export SLACK_IKIOI_TOKEN=""
+
 # channel idは https://api.slack.com/methods/channels.list/test でTest Methodボタンを押すと取れる
 export SLACK_IKIOI_CHANNEL_ID=""
 export SLACK_IKIOI_CHANNEL_NAME=""
-export SLACK_IKIOI_TOKEN=""
 
 ruby slack_ikioi_png.rb
